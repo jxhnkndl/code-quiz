3c759d62f65a616154a8ef21e79d6ed6b89e8ebb
jxhnkndl <jkroyston@gmail.com>
2020-12-11 19:15:18 -0500

    Relocate quiz data array to new file to separate quiz logic from quiz data

---
+++ data.js
@@ -0,0 +1,52 @@
+// Quiz Question Data
+var quizData = [
+  {
+    question: "Which of the following is not a valid data type in JavaScript?",
+    answers: [
+      "Numbers",
+      "Booleans",
+      "Variables",
+      "Strings"
+    ],
+    correctIndex: 2
+  }, 
+  {
+    question: "What is the correct term for a discrete, reusable block of code designed to accomplish a specific task?",
+    answers: [
+      "Conditional",
+      "Function",
+      "Prompt",
+      "Event"
+    ],
+    correctIndex: 1
+  },
+  {
+    question: "Which of the following could best be described as an indexed collection of elements?",
+    answers: [
+      "List",
+      "Batch",
+      "Object",
+      "Array"
+    ],
+    correctIndex: 3
+  },
+  {
+    question: "Which of the following is not a valid type JavaScript event?",
+    answers: [
+      "swipe",
+      "click",
+      "submit",
+      "keyup"
+    ],
+    correctIndex: 0
+  },
+  {
+    question: "What is the object that allows JavaScript to access, interact with, and manipulate elements in an HTML document in the browser?",
+    answers: [
+      "Node.js",
+      "DOM",
+      "querySelector",
+      "jQuery"
+    ]
+  }
+];
